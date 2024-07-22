16878f3fd3292d0f2652cad8e0ddf1cceb38c7e0
Shridhar Aware <ashridhar19@gmail.com>
2024-07-22 16:00:01 +0530

    Cards and passing data from express to client/ejs

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-
+const FeaturesArticles = require("./public/Featured");
 const port = process.env.PORT;
 
 const app = express();
@@ -11,9 +11,12 @@ app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + "/node_modules/bootstrap"));
 
 app.get("/", (req, res) => {
-  res.render(__dirname + "/views/index");
+  res.render(__dirname + "/views/Home", { FeaturesArticles });
 });
 
+app.get("/categories", (req, res) => {
+  res.render(__dirname + "/views/Categories");
+});
 app.get("/about", (req, res) => {
   res.render(__dirname + "/views/About");
 });
---
+++ public/Featured.js
@@ -0,0 +1,18 @@
+const FeaturesArticles = [
+  {
+    img: "https://images.pexels.com/photos/4400679/pexels-photo-4400679.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    title: "Health",
+    description: "This is health",
+  },
+  {
+    img: "https://images.pexels.com/photos/4400679/pexels-photo-4400679.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    title: "Sports",
+    description: "This is sports",
+  },
+  {
+    img: "https://images.pexels.com/photos/4400679/pexels-photo-4400679.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    title: "Technology",
+    description: "This is technology",
+  },
+];
+module.exports = FeaturesArticles;
